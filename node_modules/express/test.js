501774132d9be1f469cb4e7f94c7d5f2c34ba9ed
Bobby ENY <bobby@envisageny.com>
2014-12-01 23:21:02 +0000

    removed node_modules and Bower_compnents using .gitignore

--- node_modules/express/test.js
+++
@@ -1,8 +0,0 @@
-var app = require('./index')()
-app.get('/iamaverylongcomponent/in_a_url/you_said_didnt_work', function (req, res) {
-  res.send('got long url')
-})
-app.get('/IamUSINGcaps', function (req, res) {
-  res.send('got caps matching')
-})
-app.listen(3333)
